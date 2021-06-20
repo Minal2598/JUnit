14445912c64062cbc7d2444fe719778b3661c01f
Minal <patilminalp@gmail.com>
2021-06-21 03:08:23 +0530

    Added:This is updated utility class with day of week

--- Utility.java
+++ Utility.java
@@ -38,4 +38,17 @@ public class Utility {
         note(value);
     }
 
+           //Day Of Week//
+
+    public static int dayOfWeek(int d,int m,int y)
+    {
+
+        int y0,x,m0,d0;
+
+        y0=y-(14-m)/12;
+        x=y0+(y0/4)-(y0/100)+(y0/400);
+        m0=m+12*((14-m)/12)-2;
+        return d0=(d+x+31*m0/12)%7;
+
+    }
 }
