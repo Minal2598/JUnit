7757376ccb1b1cc1163286f862a9f0ce5e8b2395
Minal <patilminalp@gmail.com>
2021-06-21 02:53:04 +0530

    Added:Updated Utility with vending machine

--- Utility.java
+++ Utility.java
@@ -8,4 +8,34 @@ public class Utility {
     public static int getUserSpecificInteger() {
         return sc.nextInt();
     }
-  }
+    
+    public static double getUserSpecificDouble() {
+        return sc.nextDouble();
+    }
+
+            //Vending Machine//
+    static int[] notes = {1000, 500, 100, 50, 10, 5, 2, 1};
+    static int total, i;
+    static void note(int value)
+
+
+    {
+
+        if(value/notes[i] != 0)
+        {
+            total += (value/notes[i]);
+            System.out.println(total);
+            System.out.println(notes[i]+" is notes "+value/notes[i]);
+            value = value % notes[i];
+
+        }
+        i++;
+        if(value == 0)
+        {
+            System.out.println("Total notes :: "+total);
+            return;
+        }
+        note(value);
+    }
+
+}
